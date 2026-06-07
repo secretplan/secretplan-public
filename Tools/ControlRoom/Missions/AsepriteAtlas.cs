aa4f5eb269868a62005b3a3c59cd6f3c1f340d9e
notexplosive <thenotexplosive@gmail.com>
2026-06-06 22:03:05 -0700

    [Automated:ControlRoom] Updated as of 6/6/2026 10:03:05 PM

---
+++ Tools/ControlRoom/Missions/AsepriteAtlas.cs
@@ -0,0 +1,22 @@
+﻿using ControlRoom.Core;
+using ControlRoom.Programs;
+using SecretPlanCore.Core;
+
+namespace ControlRoom.Missions;
+
+public class AsepriteAtlas : Mission
+{
+    public AsepriteAtlas(List<string> rawArgs) : base(rawArgs)
+    {
+    }
+
+    public override async Task Run()
+    {
+        var fileNames = MissionVariables.GameDirectoryFiles().GetFilesAt("Art/Aseprite");
+
+        await OutPipe.AgentLogMessage($"Atlasing {fileNames.Count} files");
+
+        var aseprite = new AsepriteProgram(MissionVariables.GameDirectory);
+        await aseprite.BuildAtlas(fileNames, "Art/FullAtlas");
+    }
+}
